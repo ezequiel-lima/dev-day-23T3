a5c1fdc4a788b0c2dea5f55c6a051c40f036cb2b
0220482022039 <0220482022039@LAB1MAQ14>
2023-06-02 21:51:36 -0300

    Add project files.

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string TaxId { get; set; }
+    }
+}
---
+++ Infra.Data/DevDayDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data
+{
+    public class DevDayDataContext : DbContext
+    {
+        public DevDayDataContext(DbContextOptions<DevDayDataContext> options) : base (options)
+        {            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new CustomerMap);
+        }
+    }
+}
